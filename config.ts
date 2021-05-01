2b20cbd3bf48a3fbecb205ddd0f735c7413b44cd
Rahul Halder <hrahul2605@gmail.com>
2021-05-02 04:49:22 +0530

    typescript support added, update methods on server

---
+++ config.ts
@@ -0,0 +1,107 @@
+import { types } from "mediasoup";
+
+interface webRtcTransportOptions extends types.WebRtcTransportOptions {
+  maxIncomingBitrate: number;
+}
+
+const config: {
+  port: number;
+  mediasoup: {
+    workerSettings: types.WorkerSettings;
+    routerOptions: types.RouterOptions;
+    webRtcTransportOptions: webRtcTransportOptions;
+  };
+} = {
+  port: 3000,
+  mediasoup: {
+    // mediasoup WorkerSettings.
+    // See https://mediasoup.org/documentation/v3/mediasoup/api/#WorkerSettings
+    workerSettings: {
+      rtcMinPort: 10000,
+      rtcMaxPort: 10100,
+      logLevel: "warn",
+      logTags: [
+        "info",
+        "ice",
+        "dtls",
+        "rtp",
+        "srtp",
+        "rtcp",
+        // 'rtx',
+        // 'bwe',
+        // 'score',
+        // 'simulcast',
+        // 'svc'
+      ],
+    },
+    // mediasoup Router options.
+    // See https://mediasoup.org/documentation/v3/mediasoup/api/#RouterOptions
+    routerOptions: {
+      mediaCodecs: [
+        {
+          kind: "audio",
+          mimeType: "audio/opus",
+          clockRate: 48000,
+          channels: 2,
+        },
+        {
+          kind: "video",
+          mimeType: "video/VP8",
+          clockRate: 90000,
+          parameters: {
+            "x-google-start-bitrate": 1000,
+          },
+        },
+        {
+          kind: "video",
+          mimeType: "video/VP9",
+          clockRate: 90000,
+          parameters: {
+            "profile-id": 2,
+            "x-google-start-bitrate": 1000,
+          },
+        },
+        {
+          kind: "video",
+          mimeType: "video/h264",
+          clockRate: 90000,
+          parameters: {
+            "packetization-mode": 1,
+            "profile-level-id": "4d0032",
+            "level-asymmetry-allowed": 1,
+            "x-google-start-bitrate": 1000,
+          },
+        },
+        {
+          kind: "video",
+          mimeType: "video/h264",
+          clockRate: 90000,
+          parameters: {
+            "packetization-mode": 1,
+            "profile-level-id": "42e01f",
+            "level-asymmetry-allowed": 1,
+            "x-google-start-bitrate": 1000,
+          },
+        },
+      ],
+    },
+    // mediasoup WebRtcTransport options for WebRTC endpoints (mediasoup-client,
+    // libmediasoupclient).
+    // See https://mediasoup.org/documentation/v3/mediasoup/api/#WebRtcTransportOptions
+    webRtcTransportOptions: {
+      listenIps: [
+        {
+          ip: "127.0.0.1",
+          announcedIp: undefined,
+        },
+      ],
+      initialAvailableOutgoingBitrate: 1000000,
+      // minimumAvailableOutgoingBitrate: 600000,
+      maxSctpMessageSize: 262144,
+      // Additional options that are not part of WebRtcTransportOptions.
+      maxIncomingBitrate: 1500000,
+    },
+  },
+};
+
+export default config;
---
+++ index.ts
@@ -0,0 +1,146 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { createWorker, types } from "mediasoup";
+
+import config from "./config";
+
+let worker: types.Worker;
+let producer: types.Producer;
+let consumer: types.Consumer;
+let producerTransport: types.WebRtcTransport;
+let consumerTransport: types.WebRtcTransport;
+let router: types.Router;
+
+const app = express();
+app.use(express.json());
+app.use(express.static(__dirname));
+
+const httpServer = createServer(app);
+httpServer.listen(config.port, () => {
+  console.log(`Server Started at PORT:${config.port}`);
+});
+
+const io = new Server(httpServer, {
+  serveClient: false,
+});
+
+io.on("connection", (socket) => {
+  console.log("Connection established");
+
+  socket.on("getRouterRTPCapibilities", async (_, cb) => {
+    cb(router.rtpCapabilities);
+  });
+
+  socket.on("createProducerTransport", async (_, cb) => {
+    try {
+      const { transport, data } = await createWebRtcTransport();
+
+      producerTransport = transport;
+      cb(data);
+    } catch (err) {
+      console.log("ERR:", err);
+      cb({ err: err.message });
+    }
+  });
+
+  socket.on("createConsumerTransport", async (_, cb) => {
+    try {
+      const { transport, data } = await createWebRtcTransport();
+
+      consumerTransport = transport;
+      cb(data);
+    } catch (err) {
+      console.log("ERR:", err);
+      cb({ err: err.message });
+    }
+  });
+
+  socket.on("connectProducerTransport", async (args, cb) => {
+    await producerTransport.connect({ dtlsParameters: args.dtlsParameters });
+    cb();
+  });
+
+  socket.on("connectConsumerTransport", async (args, cb) => {
+    await consumerTransport.connect({ dtlsParameters: args.dtlsParameters });
+    cb();
+  });
+
+  socket.on("produce", async (args, cb) => {
+    const { kind, rtpParameters } = args;
+    producer = await producerTransport.produce({ kind, rtpParameters });
+    cb({ id: producer.id });
+
+    socket.broadcast.emit("newProducer");
+  });
+
+  socket.on("consume", async (args, cb) => {
+    try {
+      consumer = await consumerTransport.consume({
+        producerId: producer.id,
+        rtpCapabilities: args.rtpCapabilities,
+        paused: true,
+      });
+
+      cb({
+        producerId: producer.id,
+        consumerId: consumer.id,
+        kind: consumer.kind,
+        rtpParameters: consumer.rtpParameters,
+        type: consumer.type,
+        producerPaused: consumer.producerPaused,
+      });
+    } catch (err) {
+      console.log("ERR:", err);
+      cb({ err });
+    }
+  });
+
+  socket.on("resumeConsumer", async (_, cb) => {
+    await consumer.resume();
+    cb();
+  });
+});
+
+const createWebRtcTransport = async () => {
+  const transport = await router.createWebRtcTransport({
+    ...config.mediasoup.webRtcTransportOptions,
+  });
+
+  transport.setMaxIncomingBitrate(
+    config.mediasoup.webRtcTransportOptions.maxIncomingBitrate
+  );
+  return {
+    transport,
+    data: {
+      id: transport.id,
+      iceParameters: transport.iceParameters,
+      iceCandidates: transport.iceCandidates,
+      dtlsParameters: transport.dtlsParameters,
+    },
+  };
+};
+
+(async () => {
+  try {
+    worker = await createWorker({
+      logLevel: config.mediasoup.workerSettings.logLevel,
+      logTags: config.mediasoup.workerSettings.logTags,
+      rtcMinPort: config.mediasoup.workerSettings.rtcMinPort,
+      rtcMaxPort: config.mediasoup.workerSettings.rtcMaxPort,
+    });
+
+    console.log("Worker Created");
+
+    worker.on("died", () => {
+      console.error("Worker Died");
+      process.exit(1);
+    });
+
+    const mediaCodecs = config.mediasoup.routerOptions.mediaCodecs;
+    router = await worker.createRouter({ mediaCodecs });
+    console.log("Router Created");
+  } catch (e) {
+    console.log(e);
+  }
+})();
