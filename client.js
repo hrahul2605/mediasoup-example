3cea31cca91f6c529a4277eda0b213534b4965ad
Rahul Halder <hrahul2605@gmail.com>
2021-05-02 04:50:08 +0530

    added client connection methods, and index.html

---
+++ client.js
@@ -0,0 +1,128 @@
+const mediasoup = require("mediasoup-client");
+const io = require("socket.io-client");
+
+const connectBtn = document.querySelector("#connectBtn");
+const joinBtn = document.querySelector("#joinBtn");
+const shareWebcamBtn = document.querySelector("#shareWebcam");
+
+connectBtn.addEventListener("click", () => {
+  connect();
+});
+
+joinBtn.addEventListener("click", () => {
+  join();
+});
+
+shareWebcamBtn.addEventListener("click", () => {
+  shareWebcam();
+});
+
+let mediasoupDevice;
+let socket;
+let producer;
+
+const getMedia = async () => {
+  return await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  });
+};
+
+async function connect() {
+  socket = io();
+  socket.request = function request(type, data = {}) {
+    return new Promise((resolve) => {
+      socket.emit(type, data, resolve);
+    });
+  };
+  socket.on("connect", async () => {
+    console.log("Connected");
+    const routerRtpCapabilities = await socket.request(
+      "getRouterRTPCapibilities"
+    );
+
+    mediasoupDevice = new mediasoup.Device();
+    await mediasoupDevice.load({ routerRtpCapabilities });
+  });
+}
+
+const shareWebcam = async () => {
+  const data = await socket.request("createProducerTransport", {
+    rtpCapabilities: mediasoupDevice.rtpCapabilities,
+  });
+
+  const transport = mediasoupDevice.createSendTransport(data);
+  transport.on("connect", async ({ dtlsParameters }, cb, errb) => {
+    socket
+      .request("connectProducerTransport", { dtlsParameters })
+      .then(cb)
+      .catch(errb);
+  });
+
+  transport.on("produce", async ({ kind, rtpParameters }, cb, eb) => {
+    try {
+      const { id } = await socket.request("produce", {
+        transportId: transport.id,
+        kind,
+        rtpParameters,
+      });
+      cb({ id });
+    } catch (err) {
+      eb(err);
+    }
+  });
+
+  let stream;
+
+  transport.on("connectionstatechange", (state) => {
+    if (state === "connected")
+      document.querySelector("#localVideo").srcObject = stream;
+  });
+
+  try {
+    stream = await getMedia();
+    const track = stream.getTracks()[0];
+    const params = { track };
+    producer = await transport.produce(params);
+  } catch (err) {}
+};
+
+const join = async () => {
+  const data = await socket.request("createConsumerTransport");
+
+  const transport = mediasoupDevice.createRecvTransport(data);
+  transport.on("connect", ({ dtlsParameters }, cb, eb) => {
+    socket
+      .request("connectConsumerTransport", {
+        transportId: transport.id,
+        dtlsParameters,
+      })
+      .then(cb)
+      .catch(eb);
+  });
+  let stream;
+
+  transport.on("connectionstatechange", async (state) => {
+    if (state === "connected") {
+      document.querySelector("#remoteVideo").srcObject = stream;
+      await socket.request("resume");
+    }
+  });
+
+  stream = await consume(transport);
+};
+
+async function consume(transport) {
+  const { rtpCapabilities } = mediasoupDevice;
+  const data = await socket.request("consume", { rtpCapabilities });
+  const { producerId, consumerId, kind, rtpParameters } = data;
+  const consumer = await transport.consume({
+    id: consumerId,
+    producerId: producerId,
+    kind,
+    rtpParameters,
+  });
+  const stream = new MediaStream();
+  stream.addTrack(consumer.track);
+  return stream;
+}
